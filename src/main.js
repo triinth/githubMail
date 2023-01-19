93e89db175dc43de80aa4a48680b351d0262b0be
triinth <triintahk@gmail.com>
2023-01-19 11:00:15 +0200

    new test

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+async function run() {
+  
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, 
+      auth: {
+        user: MAIL_USERNAME, // generated ethereal user
+        pass: MAIL_PASSWORD, // generated ethereal password
+      },
+    });
+
+     await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      });
+}
